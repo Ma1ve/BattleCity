465ece1040a9305db40525ede8e4485538838692
emabel <emabel@student.21-school.ru>
2023-08-18 02:06:59 +0300

    adding layout

--- packages/client/src/app/RootLayout.tsx
+++ packages/client/src/app/RootLayout.tsx
@@ -1,9 +1,29 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
+import { ERoutes } from './App'
+import styles from './styles/rootLayout.module.css'
 
 const RootLayout = () => {
   return (
-    <main>
+    <main className={styles.mainLayout}>
+      <header className={styles.headerLayout}>
+        Tanks 1.0
+        <menu className={styles.menuLayout}>
+          <Link to={ERoutes.INDEX}>main</Link>
+          <Link to={ERoutes.LOGIN}>login</Link>
+          <Link to={ERoutes.REGISTRATION}>registration</Link>
+          <Link to={ERoutes.PROFILE}>profile</Link>
+          <Link to={ERoutes.GAME}>game</Link>
+          <Link to={ERoutes.LEADERBOARD}>leaderboard</Link>
+          <Link to={ERoutes.FORUM}>forum</Link>
+          <Link to={ERoutes.TOPIC}>topic</Link>
+          <Link to={ERoutes.ERROR404}>page404</Link>
+          <Link to={ERoutes.ERROR500}>page500</Link>
+        </menu>
+      </header>
       <Outlet />
+      <footer className={styles.footerLayout}>
+        V.E.I.S.A development &#169;
+      </footer>
     </main>
   )
 }
