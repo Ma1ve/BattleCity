6ef90bce2ddfbe977d27ad06af9a4b39d11ebb5d
vladislav <rozovvladislav76@yandex.ru>
2023-08-27 04:26:44 +0300

    [TSK-13] Main page design
    1. Сверстал главную страницу. В описании предложено сделать мини-лендинг с описанием игры, но судя по всему нам нужна страница с меню. Не знаю правильно ли понял, опишите свои ожидания от этой страницы, что на ней должно быть

--- packages/client/src/pages/Main/index.tsx
+++ packages/client/src/pages/Main/index.tsx
@@ -1,9 +1,40 @@
+import classNames from 'classnames'
+import H1 from '../../shared/ui/H1'
 import styles from './main.module.css'
 
+type IMenuItem = {
+  id: number
+  text: string
+}
+
 const Main = () => {
+  const menuItems: IMenuItem[] = [
+    {
+      id: 1,
+      text: 'Option 1',
+    },
+    {
+      id: 2,
+      text: 'Option 2',
+    },
+    {
+      id: 3,
+      text: 'Option 3',
+    },
+    {
+      id: 4,
+      text: 'Option 4',
+    },
+  ]
+
   return (
     <div className={styles.mainWrapper}>
-      <div>Main</div>
+      <H1>Battle City</H1>
+      <div className={styles.menu}>
+        {menuItems.map((item: IMenuItem) => {
+          return <div className={styles.menuItem}>{item.text}</div>
+        })}
+      </div>
     </div>
   )
 }
