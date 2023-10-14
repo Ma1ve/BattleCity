59595f566a847e14ab5283a0a6a52f2c323355cb
Matthew Aniskevich <matsvei@pxhealthcare.com>
2023-10-14 18:23:57 +0200

    add topic, reply and comment getters

--- packages/server/src/routes/topicsRouter.ts
+++ packages/server/src/routes/topicsRouter.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from 'express'
 import { Topic } from '../../dist/models/topic.model'
+import { Comment } from '../models/forum/comment'
 
-const topicsRouter = express.Router()
+const router = express.Router()
 
-topicsRouter.post('/', async (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
   try {
     const { title, author, content } = req.body
 
@@ -11,7 +12,6 @@ topicsRouter.post('/', async (req: Request, res: Response) => {
     // @ts-ignore
     const newTopic = await Topic.create({ title, author, content })
 
-    // успешный ответ с созданным топиком
     res.status(201).json(newTopic)
   } catch (error) {
     console.error('Ошибка при создании топика:', error)
@@ -19,4 +19,32 @@ topicsRouter.post('/', async (req: Request, res: Response) => {
   }
 })
 
-export default topicsRouter
+router.get('/', async (_, res: Response) => {
+  try {
+    // все топики для получения всех топиков
+    const topics = await Topic.findAll({
+      include: [
+        {
+          model: Comment,
+          as: 'comments', // имя ассоциации
+          attributes: ['id'], // выбрка только id (количество комментариев)
+        },
+      ],
+    })
+
+    // топики с кол-вом комментов
+    const topicsWithCommentCount = topics.map(topic => ({
+      id: topic.id,
+      title: topic.title,
+      author: topic.author,
+      commentCount: topic.comments.length,
+    }))
+
+    res.status(200).json(topicsWithCommentCount)
+  } catch (error) {
+    console.error('Ошибка при получении списка топиков:', error)
+    res.status(500).json({ error: 'Ошибка сервера' })
+  }
+})
+
+export default router
